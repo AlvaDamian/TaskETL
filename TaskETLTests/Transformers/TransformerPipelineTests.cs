be78b3a5635fa55c7ab587ea1fd15ce406f4db4f
AlvaDamian <AlvaDamian@users.noreply.github.com>
2020-02-11 21:59:18 -0300

    Added .vs folder to gitignore

---
+++ TaskETLTests/Transformers/TransformerPipelineTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using TaskETL.Transformers;
+using TaskETLTests.Mock;
+
+namespace TaskETLTests.Transformers
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class TransformerPipelineTests
+    {
+        [TestMethod]
+        public void TestInitializesWithoutErrors()
+        {
+            ITransformer<BasicModel, AnotherModel> transformer = null;
+        }
+    }
+}
