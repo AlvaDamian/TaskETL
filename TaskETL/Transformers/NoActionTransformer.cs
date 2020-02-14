387b54794bcb6a4883c9291c065f69e9beb3f00b
AlvaDamian <AlvaDamian@users.noreply.github.com>
2020-02-13 23:10:43 -0300

    Changed name from SameTypeTransformer to NoActionTransformer

---
+++ TaskETL/Transformers/NoActionTransformer.cs
@@ -0,0 +1,28 @@
+﻿namespace TaskETL.Transformers
+{
+    /// <summary>
+    /// This transformer will return what it gets.
+    /// </summary>
+    /// <typeparam name="SourceAndDestinationType">Data type that will
+    /// accept and return.</typeparam>
+    public class NoActionTransformer<SourceAndDestinationType>
+        : ITransformer<SourceAndDestinationType, SourceAndDestinationType>
+    {
+        private readonly string ID;
+
+        public NoActionTransformer(string ID)
+        {
+            this.ID = ID;
+        }
+
+        public string GetID()
+        {
+            return this.ID;
+        }
+
+        public SourceAndDestinationType transform(SourceAndDestinationType source)
+        {
+            return source;
+        }
+    }
+}
