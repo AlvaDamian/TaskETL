865528fedf0e5f51282bcae9b32212a5798ca974
AlvaDamian <AlvaDamian@users.noreply.github.com>
2020-02-18 23:04:57 -0300

    Added DataBaseExtractor

---
+++ TaskETL/Extractors/DB/QueryResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace TaskETL.Extractors.DB
+{
+    /// <summary>
+    /// Basic implementation of <see cref="IQueryResult"/>.
+    /// </summary>
+    public class QueryResult : IQueryResult
+    {
+        private readonly IEnumerable<string> Columns;
+        private readonly IEnumerable<object[]> Rows;
+
+        public QueryResult(IEnumerable<string> columns, IEnumerable<object[]> rows)
+        {
+            this.Columns = columns;
+            this.Rows = rows;
+        }
+
+        public IEnumerable<string> ColumnsNames()
+        {
+            return this.Columns;
+        }
+
+        public IEnumerable<object[]> ResultRows()
+        {
+            return this.Rows;
+        }
+    }
+}
