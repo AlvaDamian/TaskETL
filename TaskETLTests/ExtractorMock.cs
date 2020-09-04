02f436f29f3cbad97ec5a94b8f343382546854b5
Damián Alva <alva.damian@yahoo.com.ar>
2020-09-04 08:41:35 -0300

    ExtractorMock can wait instructed milliseconds before returning data.

--- TaskETLTests/ExtractorMock.cs
+++ TaskETLTests/ExtractorMock.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Threading;
 using TaskETL.Extractors;
 
 namespace TaskETLTests.Mock
@@ -8,8 +8,22 @@ namespace TaskETLTests.Mock
     {
         public static string DEFAULT_ID = "ExtractorMock";
 
+        private int _wait_milliseconds = 0;
+
         private readonly string ID;
         private readonly SourceType Data;
+        public DateTime StartedAt { get; private set; }
+        public DateTime CompletedAt { get; private set; }
+        public int WaitMillisecondsToComplete {
+            get
+            {
+                return this._wait_milliseconds;
+            }
+            set
+            {
+                this._wait_milliseconds = value < 0 ? 0 : value;
+            }
+        }
 
         public bool Disposed { get; private set; }
         public bool Executed { get; private set; }
@@ -27,6 +41,7 @@ namespace TaskETLTests.Mock
             this.ID = id;
             this.Disposed = false;
             this.Executed = false;
+            this.WaitMillisecondsToComplete = 0;
         }
 
         public ExtractorMock(string id, SourceType singleData) : this(id)
@@ -36,7 +51,13 @@ namespace TaskETLTests.Mock
 
         public SourceType Extract()
         {
+            this.StartedAt = DateTime.Now;
+
+            Thread.Sleep(this.WaitMillisecondsToComplete);
+
             this.Executed = true;
+
+            this.CompletedAt = DateTime.Now;
             return this.Data;
         }
 
